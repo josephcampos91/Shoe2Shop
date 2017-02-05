dd96416a345f1fbb5bbb83bee3677a544a84849f
Joseph <Joseph@DESKTOP-77T6A17>
2017-02-05 14:03:45 -0600

    initial

---
+++ Shoe2Shop.Services.WApi/Shoe2Shop.Services.WApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Shoe2Shop.Services.WApi.WebApiApplication" Language="C#" %>
---
+++ Shoe2Shop.Services.WApi/Shoe2Shop.Services.WApi/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace Shoe2Shop.Services.WApi
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
